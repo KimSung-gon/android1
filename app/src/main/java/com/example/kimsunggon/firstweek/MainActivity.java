2864f7c11b8c19e7a862de7caa270c186e7e4dd2
KimSung-gon <gonni27@naver.com>
2015-12-05 18:55:06 +0900

    no message

---
+++ app/src/main/java/com/example/kimsunggon/firstweek/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.kimsunggon.firstweek;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button bt;
+    Context mContext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mContext = this.getApplication().getApplicationContext();
+        bt = (Button)this.findViewById(R.id.button);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+
+            public void onClick(View v) {
+                Toast.makeText(mContext, "Click Button", Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+}
